d766634e07e984c70af4de9b7c9975e9a68d9f34
陈立志 <44913268+752869839@users.noreply.github.com>
2021-09-07 10:55:29 +0800

    Update settings.py

--- zeronet_spider/settings.py
+++ zeronet_spider/settings.py
@@ -12,8 +12,6 @@
 from datetime import datetime
 from elasticsearch import Elasticsearch
 
-#proxy
-HTTP_PROXY = ["http://8.210.121.248:39903","http://139.180.220.76:4000","http://139.180.220.76:4001","http://139.180.220.76:4002","http://139.180.220.76:4003","http://139.180.220.76:4004"]
 
 # redis
 REDIS_HOST = '172.16.30.65'
